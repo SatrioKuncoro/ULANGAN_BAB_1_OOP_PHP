4ebcba849e1bbef55ecb0d4c5256e577456f0d83
JunjungHasudungan <junjunghasudungan43@gmail.com>
2022-10-06 21:17:08 +0700

    penyelesaian pada latihan Constructor

--- LatihanConstructor.php
+++ LatihanConstructor.php
@@ -8,4 +8,30 @@ class Motor {
         $this->id_machine = $id_machine;
         $this->type = $type;
     }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getIdMachine(){
+        return $this->id_machine;
+    }
+
+    public function getType(){
+        return $this->type;
+    }
+
+    public function display(){
+        echo 'Nama: ' . $this->getName() .'<br>';
+        echo 'ID Mesin: ' . $this->getIdMachine() . '<br>';
+        echo 'Type Motor: ' . $this->getType() . '<br>';
+        echo '<hr>';
+    }
 }
+
+$obj1 = new Motor(name: 'Vario', id_machine: 1245);
+$obj2 = new Motor(name: 'CB 250', id_machine: 3421, type: 'sport');
+
+echo $obj1->display();
+echo $obj2->display();
+
