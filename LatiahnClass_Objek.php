d180fa0570ca9687198d258cef88f270eec25ca1
JunjungHasudungan <junjunghasudungan43@gmail.com>
2022-10-06 12:51:27 +0700

    penambahan pada inisialisasi class objek

--- LatiahnClass_Objek.php
+++ LatiahnClass_Objek.php
@@ -9,3 +9,14 @@ class Human {
     public $major;
     public $is_active;
 }
+
+$obj = new Human();
+$obj->name = 'Hasudungan';
+$obj->age = 25;
+$obj->major = 'Rekayasa Perangkat Lunak';
+$obj->is_active = true;
+
+echo 'Name: ' . $obj->name . '<br>';
+echo 'Umur: ' . $obj->age . '<br>';
+echo 'Jurusan: ' . $obj->major . '<br>';
+echo 'Aktif: ' . $obj->is_active . '<br>';
