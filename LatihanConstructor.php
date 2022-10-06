c01575809891325cce753c6f3c4d544275846969
SatrioKuncoro <satrionugr859@gmail.com>
2022-10-06 12:57:20 +0700

    penambahan pada bagian constructor

--- LatihanConstructor.php
+++ LatihanConstructor.php
@@ -1,4 +1,11 @@
 <?php
 class Motor {
     // 3 attribute
+    public $name, $id_machine, $type;
+
+    public function __construct($name, $id_machine, $type = 'automatic'){
+        $this->name = $name;
+        $this->id_machine = $id_machine;
+        $this->type = $type;
+    }
 }
